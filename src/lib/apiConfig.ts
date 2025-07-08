f7685df7cb918bc89272987da812c8370a57b4f8
ilix-studio <ilishjyoti17@gmail.com>
2025-07-09 03:50:15 +0530

    ready for deployment

--- src/lib/apiConfig.ts
+++ src/lib/apiConfig.ts
@@ -1,7 +1,8 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const API_CONFIG = {
-  BASE_URL: "https://prapti-foundation-be.onrender.com/api",
+  BASE_URL:
+    "https://prapti-foundation-api-927740497516.asia-northeast1.run.app/api",
 };
 
 export const baseQuery = fetchBaseQuery({
